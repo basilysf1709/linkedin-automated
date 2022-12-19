8e0a41ecb8e9caa0c34067271960f19e9758696c
Basil Yusuf <basilyusuf1709@gmail.com>
2022-12-19 15:53:29 -0500

    feat: added boilerplate code

--- li-automated.py
+++ li-automated.py
@@ -1,1 +1,15 @@
-print("Hello World")
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from datetime import datetime
+
+
+driver = webdriver.Chrome()
+
+url = 'https://linkedin.com/home'
+
+driver.get(url)
+time.sleep(3)
+
+
+driver.quit()
