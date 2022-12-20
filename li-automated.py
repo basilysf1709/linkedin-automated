5af221e1789564003b355c678436f340e2bd3967
Basil Yusuf <basilyusuf1709@gmail.com>
2022-12-20 11:23:06 -0500

    Added Email and Passcode from a file

--- li-automated.py
+++ li-automated.py
@@ -8,33 +8,40 @@ from selenium.webdriver.common.by import By
 import time
 from datetime import datetime
 
+
+stepCounter = 1
 # Initialize a driver variable to run Google Chrome
 driver = webdriver.Chrome()
-print(colored('Step 1:', 'white'), colored('LinkedIn Automation Begin', 'green'))
+print(colored('Step {}:'.format(stepCounter), 'white'), colored('LinkedIn Automation Begin', 'green'))
+stepCounter += 1
 
 # Initialize a LinkedIn URL
 url_for_linkedin = 'https://www.linkedin.com/home'
 
 # Get the URL up and running
 driver.get(url_for_linkedin)
-print(colored('Step 2:', 'white'), colored('LinkedIn URL Open Succesful', 'green'))
+print(colored('Step {}:'.format(stepCounter), 'white'), colored('LinkedIn URL Open Succesful', 'green'))
 time.sleep(2)
+stepCounter += 1
 
 # Find Email and Passcode input field using xpath
 email = driver.find_element("xpath", "//input[@name = 'session_key']")
 passcode = driver.find_element("xpath", "//input[@name = 'session_password']")
 
+
+# Get Email and Passcode from a file called AuthKeys.txt
 userInfo = []
-with open("../AuthKeys.txt") as Auth:
+with open("AuthKeys.txt") as Auth:
     for line in Auth:
-        userInfo.append(line)
-print(userInfo)
+        userInfo.append(line.replace('\n', ''))
+print(colored('Step {}:'.format(stepCounter), 'white'), colored('Get Email and Passcode From a File', 'green'))
+stepCounter += 1
 
 # Enter Email and Passcode in the input field
-email.send_keys("My Email")
-passcode.send_keys("Some dfasdf")
-print(colored('Step 3:', 'white'), colored('Enter Email and Passcode In The Input Field Complete', 'green'))
-
+email.send_keys(userInfo[0])
+passcode.send_keys(userInfo[1])
+print(colored('Step {}:'.format(stepCounter), 'white'), colored('Enter Email and Passcode In The Input Field Complete', 'green'))
+stepCounter += 1
 
 
 # Quit the driver
@@ -46,6 +53,6 @@ while True:
         break
     time.sleep(1)
 driver.quit()
-print(colored('Step:', 'white'), colored('Automation Complete', 'green'))
+print(colored('Step {}:'.format(stepCounter), 'white'), colored('Automation Complete', 'green'))
 
 
